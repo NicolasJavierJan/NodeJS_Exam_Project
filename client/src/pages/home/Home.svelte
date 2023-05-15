333f62fd1107f14a87cc39097ed888ad7f28c83a
NicolasJavierJan <nicolasjavierjan@gmail.com>
2023-05-15 10:56:32 +0200

    finished frontend for sign up, login, and home. moving on to main

--- client/src/pages/home/Home.svelte
+++ client/src/pages/home/Home.svelte
@@ -1,1 +1,31 @@
-<h1> WELCOME </h1>
+<div>
+    <h1>FULL STACK DEVELOPMENT WITH NODEJS</h1>
+    <h2>EXAM PROJECT</h2>
+    <h3>BY NICOLAS JAVIER JAN (DAT21I)</h3>
+</div>
+
+<style>
+    div {
+        font-family: 'Courier';
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        position: fixed;
+        padding: 18px;
+        text-align: center;
+        animation: flicker 7.5s infinite alternate;
+        }
+
+    @keyframes flicker {
+    0%, 18%, 22%, 25%, 53%, 57%, 100% {
+    text-shadow: 
+    0 0 7px #fff,
+    0 0 10px rgb(5, 129, 157),
+    0 0 21px rgb(5, 129, 157),
+    0 0 42px rgb(5, 129, 157);
+    }
+    20%, 24%, 55% {
+    text-shadow: none;
+    }
+}
+</style>
